4a79a5b28b61d3d605f560f584bdcc3063e1c7c7
Gabriel Ledesma <gledesmab@uni.pe>
2020-06-30 22:14:51 -0500

    Creacion de un login con Android y SqlLite

---
+++ app/src/main/java/com/example/notary_acevedo/UserListActivity.java
@@ -0,0 +1,21 @@
+package com.example.notary_acevedo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UserListActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+
+        textView = findViewById(R.id.textview);
+        String emailfromIntent = getIntent().getStringExtra("EMAIL");
+        textView.setText(emailfromIntent);
+    }
+}
